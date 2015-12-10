e5ebeb84558e1873df47c2b4b59eb8fedc1ad949
Thomas Fragstein <fragstone@orion.fragstein.de>
2015-12-10 01:00:57 +0100

    Neue Site.conf für den aktuellen Master von Gluon 2015.2

--- site.mk
+++ site.mk
@@ -1,29 +1,36 @@
 GLUON_SITE_PACKAGES := \
         gluon-mesh-batman-adv-14 \
-        gluon-alfred \
-        gluon-announced \
-        gluon-autoupdater \
-        gluon-config-mode-hostname \
-        gluon-config-mode-autoupdater \
-        gluon-config-mode-mesh-vpn \
-        gluon-config-mode-geo-location \
-        gluon-config-mode-contact-info \
-        gluon-ebtables-filter-multicast \
-        gluon-ebtables-filter-ra-dhcp \
-        gluon-luci-admin \
-        gluon-luci-autoupdater \
-        gluon-luci-private-wifi \
+	gluon-alfred \
+	gluon-announced \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
         gluon-luci-portconfig \
-        gluon-next-node \
-        gluon-mesh-vpn-fastd \
-        gluon-radvd \
-        gluon-status-page \
-        iwinfo \
-        iptables \
-        haveged
+	gluon-luci-wifi-config \
+	gluon-luci-theme \
+	gluon-luci-private-wifi \
+        gluon-luci-mesh-vpn-fastd \
+	gluon-next-node \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	gluon-ssid-changer \
+	haveged \
+	iptables \
+	iwinfo
 
+GLUON_LANGS ?= en de
 
-DEFAULT_GLUON_RELEASE := 0.6.1
+DEFAULT_GLUON_RELEASE := $(GLUON_BRANCH)-$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
